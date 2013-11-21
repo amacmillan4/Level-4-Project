848afe25d56a30051465cabeca064c639280dae3
Allan Macmillan <amacmillan4@gmail.com>
2013-11-21 12:29:42 +0000

    Show is now displaying roughly what it should

--- 15-11-Project/gen/project/android/bellringing/R.java
+++ 15-11-Project/gen/project/android/bellringing/R.java
@@ -34,18 +34,19 @@ public final class R {
         public static final int towerbell_ur_256=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int btnHelp=0x7f090008;
-        public static final int btnRun=0x7f090009;
-        public static final int btnStop=0x7f090007;
-        public static final int globalView=0x7f090003;
+        public static final int aa=0x7f090002;
+        public static final int action_settings=0x7f09000c;
+        public static final int btnHelp=0x7f090009;
+        public static final int btnRun=0x7f09000a;
+        public static final int btnStop=0x7f090008;
+        public static final int globalView=0x7f090004;
         public static final int helloMoonFragment=0x7f090000;
-        public static final int imageView1=0x7f09000a;
-        public static final int l1=0x7f090004;
-        public static final int mathodShowLinLayout=0x7f090002;
+        public static final int imageView1=0x7f09000b;
+        public static final int l1=0x7f090005;
+        public static final int methodShowLinLayout=0x7f090003;
         public static final int methodShowragment=0x7f090001;
-        public static final int textView1=0x7f090005;
-        public static final int textView2=0x7f090006;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090007;
     }
     public static final class layout {
         public static final int activity_method_interaction=0x7f030000;
