6d8ac09c68393f0485de1468e4bec521dc944fbe
Allan Macmillan <amacmillan4@gmail.com>
2013-11-25 11:27:23 +0000

    Begun work on new fragment

--- 15-11-Project/gen/project/android/bellringing/R.java
+++ 15-11-Project/gen/project/android/bellringing/R.java
@@ -34,27 +34,34 @@ public final class R {
         public static final int towerbell_ur_256=0x7f02000a;
     }
     public static final class id {
-        public static final int aa=0x7f090002;
-        public static final int action_settings=0x7f09000c;
-        public static final int btnHelp=0x7f090009;
-        public static final int btnRun=0x7f09000a;
-        public static final int btnStop=0x7f090008;
-        public static final int globalView=0x7f090004;
+        public static final int aa=0x7f090007;
+        public static final int action_settings=0x7f090011;
+        public static final int btnHelp=0x7f09000e;
+        public static final int btnRun=0x7f09000f;
+        public static final int btnStop=0x7f09000d;
+        public static final int globalView=0x7f090009;
         public static final int helloMoonFragment=0x7f090000;
-        public static final int imageView1=0x7f09000b;
-        public static final int l1=0x7f090005;
-        public static final int methodShowLinLayout=0x7f090003;
-        public static final int methodShowragment=0x7f090001;
-        public static final int textView1=0x7f090006;
-        public static final int textView2=0x7f090007;
+        public static final int imageView1=0x7f090010;
+        public static final int l1=0x7f09000a;
+        public static final int listView1=0x7f090003;
+        public static final int methodSelectFragment=0x7f090001;
+        public static final int methodShowLinLayout=0x7f090008;
+        public static final int methodShowragment=0x7f090002;
+        public static final int switch1=0x7f090004;
+        public static final int switch2=0x7f090006;
+        public static final int switch3=0x7f090005;
+        public static final int textView1=0x7f09000b;
+        public static final int textView2=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_method_interaction=0x7f030000;
-        public static final int activity_method_show=0x7f030001;
-        public static final int fragment_activity_show=0x7f030002;
-        public static final int fragment_method_interaction=0x7f030003;
-        public static final int options=0x7f030004;
-        public static final int startup_screen=0x7f030005;
+        public static final int activity_method_select=0x7f030001;
+        public static final int activity_method_show=0x7f030002;
+        public static final int fragment_activity_select=0x7f030003;
+        public static final int fragment_activity_show=0x7f030004;
+        public static final int fragment_method_interaction=0x7f030005;
+        public static final int options=0x7f030006;
+        public static final int startup_screen=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f080000;
