ca73a1c79a5bf81c88ee08406d0d38c1523070ac
Allan Macmillan <amacmillan4@gmail.com>
2013-12-04 10:33:55 +0000

    Number Picker and Peal Time complete

--- 15-11-Project/src/project/android/bellringing/MethodSetupPealTimeFragment.java
+++ 15-11-Project/src/project/android/bellringing/MethodSetupPealTimeFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 
 
@@ -24,8 +23,6 @@ public class MethodSetupPealTimeFragment extends Fragment {
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_activity_start_pealtime, parent, false);
 
-		LinearLayout ll = (LinearLayout) view.findViewById(R.id.stageList);
-
 		// Create the array of numbers that will populate the numberpicker
 		final String[] hours = {"1","2","3","4","5"};
 		final String[] mins = new String[30];
@@ -33,10 +30,26 @@ public class MethodSetupPealTimeFragment extends Fragment {
 		for(int i=0; i < 30; i++) {
 			mins[i] = Integer.toString(i*2);
 		}
-
-
-
 		//set up number picker
+		final NumberPicker nmMins = (NumberPicker) view.findViewById(R.id.minutes);
+		nmMins.setWrapSelectorWheel(true);
+	    nmMins.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+		
+		if (MethodLab.get(getActivity()).getSetup().getPealTime().substring(0,1).equals("1")){
+			nmMins.setMaxValue(15);
+			nmMins.setMinValue(1);
+			nmMins.setDisplayedValues(Arrays.copyOfRange(mins, 15, 30));
+			nmMins.setValue((Integer.parseInt(MethodLab.get(getActivity()).getSetup().getPealTime().substring(2,MethodLab.get(getActivity()).getSetup().getPealTime().length())))/2 - 14);
+			
+		}
+		else{
+			nmMins.setMaxValue(30);
+			nmMins.setMinValue(1);
+			nmMins.setDisplayedValues(mins);
+			nmMins.setValue(Integer.parseInt(MethodLab.get(getActivity()).getSetup().getPealTime().substring(2,MethodLab.get(getActivity()).getSetup().getPealTime().length()))/2 + 1);
+		
+		}
+
 		final NumberPicker nmHours = (NumberPicker) view.findViewById(R.id.hours);
 		nmHours.setMaxValue(5);
 		nmHours.setMinValue(1);
@@ -44,35 +57,29 @@ public class MethodSetupPealTimeFragment extends Fragment {
 		nmHours.setDisplayedValues(hours);
 		nmHours.setValue(Integer.parseInt(MethodLab.get(getActivity()).getSetup().getPealTime().substring(0,1)));
 		nmHours.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+		nmHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+			
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 
-		final NumberPicker nmMins = (NumberPicker) view.findViewById(R.id.minutes);
-		nmMins.setMaxValue(30);
-		nmMins.setMinValue(1);
-		nmMins.setWrapSelectorWheel(true);
-		nmMins.setDisplayedValues(mins);
-		nmMins.setValue(Arrays.asList(mins).indexOf(Integer.parseInt(MethodLab.get(getActivity()).getSetup().getPealTime().substring(2,MethodLab.get(getActivity()).getSetup().getPealTime().length()))));
-		nmMins.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
-		//		nmMins.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-		//
-		//			@Override
-		//			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-		//				// TODO Auto-generated method stub
-		//
-		//
-		// TODO Auto-generated method stub
-		//				if (nmHours.getValue() == 1){
-		//					//nmMins.setDisplayedValues(Arrays.copyOfRange(mins, 15, 30));
-		//					nmMins.setMaxValue(30);
-		//					nmMins.setMaxValue(15);
-		//					nmMins.setValue(30);
-		//				}
-		//				else{
-		//					nmMins.setDisplayedValues(mins);
-		//					nmMins.setMaxValue(30);
-		//					nmMins.setMinValue(1);
-		//				}	
-		//			}
-		//		});
+				//TODO Auto-generated method stub
+				if (nmHours.getValue() == 1){
+					
+					nmMins.setValue(1);
+					nmMins.setMaxValue(15);
+					nmMins.setMinValue(1);
+					nmMins.setDisplayedValues(Arrays.copyOfRange(mins, 15, 30));
+					
+				}
+				else {
+					nmMins.setDisplayedValues(mins);
+					nmMins.setValue(nmMins.getValue());
+					nmMins.setMaxValue(30);
+					nmMins.setMinValue(1);
+				}	
+			}
+		});
+
 
 		Button btnPeal = (Button) view.findViewById(R.id.btnPealTime);
 		btnPeal.setOnClickListener(new View.OnClickListener() {
@@ -81,11 +88,17 @@ public class MethodSetupPealTimeFragment extends Fragment {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				String minutes;
-				if (Integer.parseInt(mins[nmMins.getValue()-1]) < 10)
-					minutes = "0" + mins[nmMins.getValue()-1];
-				else
-					minutes = mins[nmMins.getValue()-1] + "";
+				if (nmHours.getValue() == 1){
+					minutes = ((nmMins.getValue() - 1) * 2) + 30 + "";
+				}
+				else{
+					if (Integer.parseInt(mins[nmMins.getValue()-1]) < 10)
+						minutes = "0" + mins[nmMins.getValue()-1];
+					else
+						minutes = mins[nmMins.getValue()-1] + "";
+				}
 				
+
 				MethodLab.get(getActivity()).getSetup().setPealTime(nmHours.getValue() + ":" + minutes);
 				getActivity().finish();
 
