b8f47415223bf7ca752a078253a95af274aea622
Allan Macmillan <amacmillan4@gmail.com>
2014-01-16 14:23:27 +0000

    Solved stand button bug

--- 15-11-Project/src/project/android/bellringing/MethodStatus.java
+++ 15-11-Project/src/project/android/bellringing/MethodStatus.java
@@ -2,6 +2,6 @@ package project.android.bellringing;
 
 public enum MethodStatus {
 	
-	STANDING, GO_TO_ROUNDS, ROUNDS, GO_TO_PLAYING, PLAYING, GO_TO_STAND
+	STANDING, GO_TO_ROUNDS, ROUNDS, GO_TO_PLAYING, PLAYING, GO_TO_STAND, GO_TO_STAND_FROM_ROUNDS
 	
 }
